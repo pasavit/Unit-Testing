555621fb131e866d7b6ca7c81bef92911a581bbc
unknown <meekhunsutp@gmail.com>
2021-03-06 19:06:05 -0600

    Created Math Tests, Add_WhenCalled_ReturnSumOfArguments

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class MathTests
+    {
+        [TestMethod]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            var math = new Fundamentals.Math();
+
+            var result = math.Add(1, 2);
+
+            Assert.AreEqual(3 , result);
+        }
+
+
+    }
+}
