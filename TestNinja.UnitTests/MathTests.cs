b76f95cbb14cdfb994e4e3acc0f0f06f9f5d2779
unknown <meekhunsutp@gmail.com>
2021-03-06 19:15:22 -0600

    Created tests for each execution paths for Max Math method

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -21,6 +21,35 @@ namespace TestNinja.UnitTests
             Assert.AreEqual(3 , result);
         }
 
+        [TestMethod]
+        public void Max_FirstArgumentIsGreater_ReturnFirstArgument()
+        {
+            var math = new Fundamentals.Math();
+
+            var result = math.Max(2, 1);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Max_SecondArgumentIsGreater_ReturnSecondArgument()
+        {
+            var math = new Fundamentals.Math();
+
+            var result = math.Max(1, 2);
+
+            Assert.AreEqual(2, result);
+        }
+        
+        [TestMethod]
+        public void Max_ArgumentAreEqual_ReturnSameArgument()
+        {
+            var math = new Fundamentals.Math();
+
+            var result = math.Max(1, 1);
+
+            Assert.AreEqual(1, result);
+        }
 
     }
 }
