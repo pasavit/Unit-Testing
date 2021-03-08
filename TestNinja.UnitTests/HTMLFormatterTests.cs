f7bacbc17cf834585fd2752fa757b5ce6bbd2202
unknown <meekhunsutp@gmail.com>
2021-03-07 19:08:12 -0600

    HTMLFormatterTests SetUp

---
+++ TestNinja.UnitTests/HTMLFormatterTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HTMLFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_shouldEncloseStringWithStrongElement()
+        {
+            var formatter = new HtmlFormatter();
+
+            var result = formatter.FormatAsBold("abc");
+
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>"));
+
+        }
+    }
+}
