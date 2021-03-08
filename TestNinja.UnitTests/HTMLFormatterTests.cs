09c648ca1b11ff0318b660fde046bb69f5a0096e
unknown <meekhunsutp@gmail.com>
2021-03-07 19:12:25 -0600

    Testing strings with specific and general asserts

--- TestNinja.UnitTests/HTMLFormatterTests.cs
+++ TestNinja.UnitTests/HTMLFormatterTests.cs
@@ -18,7 +18,13 @@ namespace TestNinja.UnitTests
 
             var result = formatter.FormatAsBold("abc");
 
+            //specific
             Assert.That(result, Is.EqualTo("<strong>abc</strong>"));
+
+            //general
+            Assert.That(result, Does.StartWith("<strong>"));
+            Assert.That(result, Does.EndWith("</strong>"));
+            Assert.That(result, Does.Contain("abc"));
 
         }
     }
