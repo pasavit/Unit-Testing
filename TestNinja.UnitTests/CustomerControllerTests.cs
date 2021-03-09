2a637796d54646af98bf3130da9dc43d331b9f5d
unknown <meekhunsutp@gmail.com>
2021-03-08 20:06:33 -0600

    Assert.That(result, Is.InstanceOf<Ok>())

--- TestNinja.UnitTests/CustomerControllerTests.cs
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
 {
-    class CustomerControllerTests
+    [TestFixture]
+    public class CustomerControllerTests
     {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(0);
+
+            NUnit.Framework.Assert.That(result, Is.TypeOf<NotFound>());
+            //NUnit.Framework.Assert.That(result, Is.InstanceOf<NotFound>());
+            //InstanceOf can be a not found object or one of its derivatives
+            //TypeOf ensures that the result is exactly the not found object
+        }
+        
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(1);
+
+            NUnit.Framework.Assert.That(result, Is.InstanceOf<Ok>());
+        }
+
+
     }
 }
